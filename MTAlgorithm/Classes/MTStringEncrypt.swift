a8ccbd656f04de420ba8bc1402c7e02d0d965d9f
pulei yu <puleiyu@puleideMac-mini.local>
2023-05-23 14:38:55 +0800

    String Encrypt

---
+++ MTAlgorithm/Classes/MTStringEncrypt.swift
@@ -0,0 +1,28 @@
+//
+//  MTStringEncrypt.swift
+//  MTAlgorithm
+//
+//  Created by pulei yu on 2023/5/23.
+//
+
+import Foundation
+extension String {
+    public func mtAES256Encryt() -> String? {
+        guard let data = self.data(using: .utf8) as NSData? else { return nil }
+
+        let cStr = cString(using: .utf8)
+        let resultData = NSData(bytes: cStr, length: data.length)
+        let encrypteStr = (resultData as Data).mtAES256Encrypt()
+        return encrypteStr
+    }
+
+    public func mtAES256Decrypt() -> String? {
+        if count < 20 { return nil }
+
+        let key = (self as NSString).substring(to: 20)
+        let content = (self as NSString).substring(from: 20)
+        guard let data = Data(base64Encoded: content, options: .init(rawValue: 0)) else { return nil }
+        let result = data.mtAES256Decrypt(key)
+        return result
+    }
+}
